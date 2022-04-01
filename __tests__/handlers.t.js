ff1b5c2b2e7efe686a41d52566aec8ee87821db2
Wayne <wayne.wood@colorado.edu>
2022-03-31 19:39:57 -0600

    clean/comment

--- __tests__/handlers.t.js
+++ __tests__/handlers.t.js
@@ -1,16 +1,27 @@
-const routes = require('../routes/api.js');
-const got = require('got');
+/** Express router providing pull request related routes
+ * @requires app - Express app object
+ * @requires server - Listening server instance
+ * @requires supertest - SuperAgent Http tester
+ */
 const { app, server } = require('../index.js') 
 const supertest = require('supertest')
 const request = supertest(app)
 
 jest.setTimeout(9000);
 
+/**
+ * Process to run after testing suite is complete
+ * @name afterAll
+ * @param1 done - method to close jest instance after async/await
+ */
 afterAll(done => {
     server.close();
     done();
 });
 
+/**
+ * Test suite for pull request for assertion
+ */
 describe('Test route', function () {
     test('responds to /pulls', async () => {
         const res = await request.get('/api/pulls?owner=google&repo=googletest&page_number=1&per_page=1');
